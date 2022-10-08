4a026f1002ba313e438b66efc64a1685bf249add
Linus Zhang <l625zhan@uwaterloo.ca>
2022-10-07 22:31:57 -0400

    Init

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,3 +1,19 @@
-fun main() {
-    println("Hello, Kotlin/Native!")
+fun main(args: Array<String>) {
+    when (args.first()) {
+        "add" -> {
+
+        }
+
+        "list" -> {
+
+        }
+
+        "delete" -> {
+
+        }
+
+        else -> {
+            println("Unrecognized command.")
+        }
+    }
 }
