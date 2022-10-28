339a448a9ab75fbc17a6e0a08e042b458261b8ed
Linus Zhang <l625zhan@uwaterloo.ca>
2022-10-28 11:22:33 -0400

    Fixed serialization issues

--- src/main/kotlin/data/TodoItem.kt
+++ src/main/kotlin/data/TodoItem.kt
@@ -2,6 +2,7 @@ package data
 
 import data.TodoCategories.clientDefault
 import edu.uwaterloo.cs.todo.lib.ItemImportance
+import edu.uwaterloo.cs.todo.lib.TodoItemModel
 import kotlinx.datetime.*
 import kotlinx.datetime.TimeZone
 import org.jetbrains.exposed.dao.IntEntity
@@ -9,25 +10,28 @@ import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import java.util.*
 
-class TodoItem(id: EntityID<Int>) : IntEntity(id), edu.uwaterloo.cs.todo.lib.TodoItem {
+class TodoItem(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<TodoItem>(TodoItems)
 
-    override val uniqueId by TodoItems.uniqueId.clientDefault { UUID.randomUUID() }
-    override var name by TodoItems.name
-    override var description by TodoItems.description
-    override var importance: ItemImportance by TodoItems.importance.transform(
+    val uniqueId by TodoItems.uniqueId.clientDefault { UUID.randomUUID() }
+    var name by TodoItems.name
+    var description by TodoItems.description
+    var importance: ItemImportance by TodoItems.importance.transform(
         { it.ordinal },
         { ItemImportance.values()[it] }
     )
-    override var categoryId by TodoItems.categoryId
-    override var modifiedTime: LocalDateTime by TodoItems.modifiedTime
+    var categoryId by TodoItems.categoryId
+    var modifiedTime: LocalDateTime by TodoItems.modifiedTime
         .clientDefault { Clock.System.now().epochSeconds }
         .transform(
             { it.toInstant(TimeZone.currentSystemDefault()).epochSeconds },
             { Instant.fromEpochSeconds(it).toLocalDateTime(TimeZone.currentSystemDefault()) }
         )
-    override var deadline: LocalDate? by TodoItems.deadline.transform(
+    var deadline: LocalDate? by TodoItems.deadline.transform(
         { it?.toEpochDays() },
-        { if(it === null) null else LocalDate.fromEpochDays(it) }
+        { if (it === null) null else LocalDate.fromEpochDays(it) }
     )
+
+    fun toModel(): TodoItemModel =
+        TodoItemModel(uniqueId, name, description, categoryId, importance, deadline, modifiedTime)
 }
