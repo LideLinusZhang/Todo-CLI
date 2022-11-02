b46f910269223695fac4cc5b53c421e3c8785116
Linus Zhang <l625zhan@uwaterloo.ca>
2022-11-02 14:28:07 -0400

    Fixed JSON serialization issue related with sync

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,29 @@
 import com.github.ajalt.clikt.core.NoOpCliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import com.sksamuel.hoplite.ConfigLoaderBuilder
-import com.sksamuel.hoplite.addResourceSource
-import com.sksamuel.hoplite.json.JsonPropertySource
+import com.sksamuel.hoplite.addFileSource
 import data.DataFactory
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.runBlocking
 import sync.SyncServerConfig
 import sync.SyncService
+import java.io.File
 
 class Cli : NoOpCliktCommand()
 
 fun main(args: Array<String>) {
     val factory = DataFactory("jdbc:sqlite:./data.db")
-    val config = ConfigLoaderBuilder.default()
-        .addSource(JsonPropertySource(""" { "serverURL": "localhost:8080", "enabled": false } """))
-        .addResourceSource("/config.json", optional = true)
-        .build()
-        .loadConfigOrThrow<SyncServerConfig>()
-    val client = HttpClient(CIO)
+    val configFile = File("config.json")
+    val config: SyncServerConfig = if (configFile.exists()) {
+        ConfigLoaderBuilder.default()
+            .addFileSource(configFile)
+            .build()
+            .loadConfigOrThrow()
+    } else SyncServerConfig("", false)
+    val client = HttpClient(CIO) { install(ContentNegotiation) { json() } }
     val syncService = SyncService(client, config.enabled, config.serverURL)
 
     runBlocking { syncService.syncDatabase(factory) }
