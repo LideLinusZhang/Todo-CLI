50712d3a950f724f9fc7e9a374b5f2da12b9a88d
DANTHINKPAD\25083 <zhichen.ni@uwaterloo.ca>
2022-11-03 02:54:59 -0400

    New Unit test, Readme and exception and test

--- src/test/kotlin/DeleteItemTest.kt
+++ src/test/kotlin/DeleteItemTest.kt
@@ -1,5 +1,9 @@
 import data.DataFactory
+import data.TodoCategories
+import data.TodoCategory
+import data.TodoItem
 import exceptions.IdNotFoundException
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
@@ -19,4 +23,29 @@ internal class DeleteItemTest {
 
         assertThrowsExactly(IdNotFoundException::class.java) { command.parse(arrayOf("1")) }
     }
+
+    @Test
+    fun DeleteSuccess_CategoryAndItemAllMatch() {
+        // Arrange
+        val dataFactory = DataFactory()
+        val command = DeleteItem(dataFactory)
+
+
+       dataFactory.transaction {
+           val category = TodoCategory.new {
+               name = "Physics"
+               favoured = true
+           }
+           TodoItem.new {
+               name = "A1"
+               categoryId = category.uniqueId
+           }
+           assertDoesNotThrow { command.parse(arrayOf(("1")))}
+           assertNull(TodoItem.findById(1))
+       }
+
+
+    }
 }
+
+
