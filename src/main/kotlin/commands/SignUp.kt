738f18b6ff5d9604684ff5b50a0e3309cb3b0336
Linus Zhang <l625zhan@uwaterloo.ca>
2022-11-14 14:44:58 -0500

    Added special options for GUI to perform user registration

--- src/main/kotlin/commands/SignUp.kt
+++ src/main/kotlin/commands/SignUp.kt
@@ -2,13 +2,23 @@ package commands
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.PrintMessage
+import com.github.ajalt.clikt.parameters.groups.OptionGroup
+import com.github.ajalt.clikt.parameters.groups.cooccurring
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.mordant.terminal.Terminal
 import edu.uwaterloo.cs.todo.lib.getHashedPassword
 import kotlinx.coroutines.runBlocking
 import sync.CloudService
 
 class SignUp(private val cloudService: CloudService?) : CliktCommand("Create an account for synchronization.") {
+    private val userCredentialOptions by UserCredentialOptions().cooccurring()
     private val terminal = Terminal()
+
+    private class UserCredentialOptions : OptionGroup() {
+        val userName by option("--username", hidden = true).required()
+        val password by option("--password", hidden = true).required()
+    }
 
     override fun run() {
         if (cloudService === null)
@@ -17,8 +27,15 @@ class SignUp(private val cloudService: CloudService?) : CliktCommand("Create an 
                 error = true
             )
 
-        val userName = prompt("Username")!!
-        val password = prompt("Password", hideInput = true, requireConfirmation = true)!!
+        val userName: String
+        val password: String
+        if (userCredentialOptions === null) {
+            userName = prompt("Username")!!
+            password = prompt("Password", hideInput = true, requireConfirmation = true)!!
+        } else {
+            userName = userCredentialOptions!!.userName
+            password = userCredentialOptions!!.password
+        }
 
         val response = runBlocking { cloudService.signUp(userName, getHashedPassword(userName, password)) }
 
